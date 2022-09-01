7d8f9462619e1b56896573bc38599cd2333b3e61
SeemaMistry <seema.m.1996@gmail.com>
2022-08-31 21:58:42 -0400

    feat: initalize SecurityClearance entity

---
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/SecurityClearance.java
@@ -0,0 +1,27 @@
+package com.bugTrackerApp.BugTrackerApp.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class SecurityClearance extends AbstractEntity{
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    @NotNull
+    @JsonIgnoreProperties({"employees"})
+    private Company company;
+
+    @NotEmpty
+    private String securityTitle = "";
+
+    @NotEmpty
+    private String securityLevel = "";
+}
