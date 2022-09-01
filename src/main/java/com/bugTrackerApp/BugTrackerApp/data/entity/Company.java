d033733c5eb129c2ca8c87f6ef0d6586078fb274
SeemaMistry <seema.m.1996@gmail.com>
2022-08-31 20:16:16 -0400

    fix: add missing List import

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/Company.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/Company.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @Getter
