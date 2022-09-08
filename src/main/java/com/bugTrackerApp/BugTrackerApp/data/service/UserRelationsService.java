8f36880d73b2117d897ece7dd375e818c7ffb1e5
SeemaMistry <seema.m.1996@gmail.com>
2022-09-08 13:52:12 -0400

    feat: initialize user realtions service fields and constructor

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/UserRelationsService.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/UserRelationsService.java
@@ -1,12 +1,30 @@
 package com.bugTrackerApp.BugTrackerApp.data.service;
 
+import com.bugTrackerApp.BugTrackerApp.data.repository.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRelationsService {
     // fields
+    private CompanyRepository companyRepo;
+    private EmployeeRepository  employeeRepo;
+    private DepartmentRepository departmentRepo;
+    private SecurityClearanceRepository securityClearanceRepo;
+    private AccountStatusRepository accountStatusRepo;
 
     // constructor
+    public UserRelationsService(CompanyRepository companyRepo,
+                                EmployeeRepository employeeRepo,
+                                DepartmentRepository departmentRepo,
+                                SecurityClearanceRepository securityClearanceRepo,
+                                AccountStatusRepository accountStatusRepo) {
+        this.companyRepo = companyRepo;
+        this.employeeRepo = employeeRepo;
+        this.departmentRepo = departmentRepo;
+        this.securityClearanceRepo = securityClearanceRepo;
+        this.accountStatusRepo = accountStatusRepo;
+    }
+
 
     // methods
 }
