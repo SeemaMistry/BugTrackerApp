0de9a5bce9e67f9faeda715e35aad8d37c3b22c5
SeemaMistry <seema.m.1996@gmail.com>
2022-09-08 18:49:54 -0400

    feat: add search query to find company by name

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/CompanyRepository.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/CompanyRepository.java
@@ -2,8 +2,14 @@ package com.bugTrackerApp.BugTrackerApp.data.repository;
 
 import com.bugTrackerApp.BugTrackerApp.data.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    @Query("select c from Company c " +
+            "where lower(c.name) like lower(concat('%', :searchTerm, '%')) " )
+    List<Company> search(@Param("searchTerm") String searchTerm);
 }
