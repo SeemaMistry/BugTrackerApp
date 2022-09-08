7d30efc06805f0fa482c4b4f5eb9f122e052b83a
SeemaMistry <seema.m.1996@gmail.com>
2022-09-08 13:39:26 -0400

    feat: initialize userrelations service class

---
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/UserRelationsService.java
@@ -0,0 +1,12 @@
+package com.bugTrackerApp.BugTrackerApp.data.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRelationsService {
+    // fields
+
+    // constructor
+
+    // methods
+}
