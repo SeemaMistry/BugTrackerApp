d082cf788985822551a4354f287ab3013641ab04
SeemaMistry <seema.m.1996@gmail.com>
2022-09-09 12:00:57 -0400

    feat: initialize main layout view

---
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
@@ -0,0 +1,18 @@
+package com.bugTrackerApp.BugTrackerApp.views;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+
+public class MainLayout extends AppLayout {
+    public MainLayout() {
+        createHeader();
+        createDrawer();
+    }
+
+    private void createHeader() {
+        // TODO: make header navigation
+    }
+
+    private void createDrawer() {
+        // TODO: make menu bar navigation
+    }
+}
