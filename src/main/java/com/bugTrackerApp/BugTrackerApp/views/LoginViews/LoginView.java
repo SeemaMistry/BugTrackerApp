64ae64f0113fc2ca0d67ee0cbe805838a0192fbf
SeemaMistry <seema.m.1996@gmail.com>
2022-09-19 16:15:03 -0400

    build: comment out spring security and login page for simplicity, will add back later after other pages are built

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/LoginViews/LoginView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/LoginViews/LoginView.java
@@ -1,40 +1,40 @@
-package com.bugTrackerApp.BugTrackerApp.views.LoginViews;
-
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-
-import javax.annotation.security.PermitAll;
-
-@PageTitle("Login | Bug Tracker")
-@Route(value="login")
-@PermitAll
-public class LoginView extends VerticalLayout implements BeforeEnterObserver {
-    private final LoginForm login = new LoginForm();
-    public LoginView() {
-        addClassName("login-view");
-        setSizeFull();
-        setAlignItems(Alignment.CENTER);
-        setJustifyContentMode(JustifyContentMode.CENTER);
-
-        login.setAction("login");
-
-
-        add(new H1("Ticket System Login"), login);
-    }
-    @Override
-    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
-        if(beforeEnterEvent.getLocation()
-                .getQueryParameters()
-                .getParameters()
-                .containsKey("error")) {
-            login.setError(true);
-
-        }
-
-    }
-}
+//package com.bugTrackerApp.BugTrackerApp.views.LoginViews;
+//
+//import com.vaadin.flow.component.html.H1;
+//import com.vaadin.flow.component.login.LoginForm;
+//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+//import com.vaadin.flow.router.BeforeEnterEvent;
+//import com.vaadin.flow.router.BeforeEnterObserver;
+//import com.vaadin.flow.router.PageTitle;
+//import com.vaadin.flow.router.Route;
+//
+//import javax.annotation.security.PermitAll;
+//
+//@PageTitle("Login | Bug Tracker")
+//@Route(value="login")
+//@PermitAll
+//public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+//    private final LoginForm login = new LoginForm();
+//    public LoginView() {
+//        addClassName("login-view");
+//        setSizeFull();
+//        setAlignItems(Alignment.CENTER);
+//        setJustifyContentMode(JustifyContentMode.CENTER);
+//
+//        login.setAction("login");
+//
+//
+//        add(new H1("Ticket System Login"), login);
+//    }
+//    @Override
+//    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+//        if(beforeEnterEvent.getLocation()
+//                .getQueryParameters()
+//                .getParameters()
+//                .containsKey("error")) {
+//            login.setError(true);
+//
+//        }
+//
+//    }
+//}
