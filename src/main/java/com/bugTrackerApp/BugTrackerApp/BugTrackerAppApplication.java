91fde16853a0e58edab8ea5d632994bb01a6f545
SeemaMistry <seema.m.1996@gmail.com>
2022-09-30 16:55:13 -0400

    feat: add annotations Theme and NPMPackage to read styles.css file

--- src/main/java/com/bugTrackerApp/BugTrackerApp/BugTrackerAppApplication.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/BugTrackerAppApplication.java
@@ -1,10 +1,17 @@
 package com.bugTrackerApp.BugTrackerApp;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+@Theme(value = "BugTrackerApp") // allows reading of styles.css. Requires: SpringBootServletInitializer & AppShellConfigurator
+@NpmPackage(value = "line-awesome", version = "1.3.0") // import line awesome, url specified in style.css
 @SpringBootApplication
-public class BugTrackerAppApplication {
+public class BugTrackerAppApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BugTrackerAppApplication.class, args);
