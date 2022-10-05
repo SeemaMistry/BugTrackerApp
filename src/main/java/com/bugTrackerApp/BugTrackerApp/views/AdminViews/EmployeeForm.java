ea8a4e0571bca3347f9e3e83fb8747dba0e87274
SeemaMistry <seema.m.1996@gmail.com>
2022-10-05 17:57:11 -0400

    feat: set up component events savve, delete, cancel for EmployeeForm

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/AdminViews/EmployeeForm.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/AdminViews/EmployeeForm.java
@@ -4,16 +4,19 @@ import com.bugTrackerApp.BugTrackerApp.data.entity.AccountStatus;
 import com.bugTrackerApp.BugTrackerApp.data.entity.Company;
 import com.bugTrackerApp.BugTrackerApp.data.entity.Employee;
 import com.bugTrackerApp.BugTrackerApp.data.entity.SecurityClearance;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 
 import java.util.List;
 
@@ -63,9 +66,26 @@ public class EmployeeForm extends FormLayout {
         delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
         close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 
+        // fire events
+        save.addClickListener(event -> validateAndSave());
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, employee)));
+        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
+        // validate form each time it changes
+
         return new HorizontalLayout(save, delete, close);
 
     }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(employee);
+            fireEvent(new SaveEvent(this, employee));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
     // Employee Setter
     public void setEmployee(Employee employee) {
         this.employee = employee;
@@ -73,5 +93,42 @@ public class EmployeeForm extends FormLayout {
         binder.readBean(employee);
     }
 
+    // Events
+    public static abstract class EmployeeFormEvent extends ComponentEvent<EmployeeForm> {
+        private Employee employee;
 
-}
+        protected EmployeeFormEvent(EmployeeForm source, Employee employee) {
+            super(source, false);
+            this.employee = employee;
+        }
+
+        public Employee getEmployee() {
+            return employee;
+        }
+    }
+
+    public static class SaveEvent extends EmployeeFormEvent {
+        SaveEvent(EmployeeForm source, Employee employee) {
+            super(source, employee);
+        }
+    }
+
+    public static class DeleteEvent extends EmployeeFormEvent {
+        DeleteEvent(EmployeeForm source, Employee employee) {
+            super(source, employee);
+        }
+
+    }
+
+    public static class CloseEvent extends EmployeeFormEvent {
+        CloseEvent(EmployeeForm source) {
+            super(source, null);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+} // end of employeeForm
