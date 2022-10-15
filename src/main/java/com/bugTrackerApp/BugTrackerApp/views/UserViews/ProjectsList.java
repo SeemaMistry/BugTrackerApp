b641b28206fbaf3738d7b38948b477b53e17c506
SeemaMistry <seema.m.1996@gmail.com>
2022-10-14 21:14:07 -0400

    feat: display a grid of projects that when selected navigates to TicketList view based on project name

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/ProjectsList.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/ProjectsList.java
@@ -1,18 +1,63 @@
 package com.bugTrackerApp.BugTrackerApp.views.UserViews;
 
+import com.bugTrackerApp.BugTrackerApp.data.entity.Project;
+import com.bugTrackerApp.BugTrackerApp.data.entity.Ticket;
+import com.bugTrackerApp.BugTrackerApp.data.service.TicketSystemService;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 
 import javax.annotation.security.RolesAllowed;
+import java.awt.*;
 
 @PageTitle("Projects | Bug Tracker")
 @Route(value="projects")
 @RolesAllowed({"USER", "ADMIN"})
 public class ProjectsList extends VerticalLayout {
-    public ProjectsList() {
+    Grid<Project> grid = new Grid<>(Project.class);
+    TextField filterText = new TextField();
+    TicketSystemService TSService;
+    public ProjectsList(TicketSystemService TSService) {
+        this.TSService = TSService;
         H1 welcome = new H1("A list of all your projects");
-        add(welcome);
+
+        //configure grid
+        setSizeFull();
+        configureGrid();
+
+        add(welcome, getContent());
+        updateList();
+    }
+
+    private void updateList() {
+        grid.setItems(TSService.findAllProjects());
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid);
+        content.setSizeFull();
+        return content;
+    }
+
+    private void configureGrid() {
+        grid.setSizeFull();
+        grid.setColumns("name", "description");
+        grid.addColumn(e -> e.getProjectStatus().getName()).setHeader("Status");
+        grid.addColumn(e -> e.getCreator_employee().getFirstName()).setHeader("Creator");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        // double click and be routed to tickets page
+        grid.asSingleSelect().addValueChangeListener(
+                e -> {
+                    UI.getCurrent().navigate(TicketsList.class, e.getValue().getName());
+                }
+        );
     }
 }
