e40d5a33b1dd53adec3c8981f65bd707b6cd164c
SeemaMistry <seema.m.1996@gmail.com>
2022-10-14 20:44:59 -0400

    feat: initialize username, passwordHash and role, accountstatus is commented out for now, will implement later

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/User.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/User.java
@@ -1,14 +1,17 @@
 package com.bugTrackerApp.BugTrackerApp.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.annotation.Nullable;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -18,5 +21,35 @@ import javax.persistence.Entity;
 @AttributeOverrides({
         @AttributeOverride(name = "id", column = @Column(name = "user_id"))
 })
+// user entity containing: username, password, role, account status
+// in a 1:1 relationship with employee
 public class User extends AbstractEntity{
+    // OWNED FIELDS
+    @Column(unique = true)
+   // @NotNull
+    private String username;
+    private String passwordSalt;
+    private String passwordHash;
+
+    //@NotNull
+    private Role role;
+
+//    @ManyToOne
+//    @JoinColumn(name = "account_status_id")
+//    @NotNull
+//    @JsonIgnoreProperties({"accountStatuses"})
+//    private AccountStatus accountStatus;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.role = role;
+        this.passwordSalt = RandomStringUtils.random(32);
+        this.passwordHash = DigestUtils.sha1Hex(password + passwordSalt);
+    }
+
+    public boolean checkPassword(String password) {
+        return DigestUtils.sha1Hex(password + passwordSalt).equals(passwordHash);
+    }
+
+
 }
