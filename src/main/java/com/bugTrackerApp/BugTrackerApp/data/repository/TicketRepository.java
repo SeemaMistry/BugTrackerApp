fdff28928df3f143d0a98b5b7a2e2dc652acd1c8
SeemaMistry <seema.m.1996@gmail.com>
2022-10-14 21:05:37 -0400

    feat: initialize custom search query of tickets based on project name

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/TicketRepository.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/TicketRepository.java
@@ -2,8 +2,16 @@ package com.bugTrackerApp.BugTrackerApp.data.repository;
 
 import com.bugTrackerApp.BugTrackerApp.data.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+    @Query("select t from Ticket t " +
+                 "where t.project.name = :pName"
+    )
+    List<Ticket> findTicketByProjectName(@Param("pName") String name);
+
 }
