b64af16d2ee92e6c661955d808a4f57e28c2c12c
SeemaMistry <seema.m.1996@gmail.com>
2022-11-02 18:45:50 -0400

    feat: rename addSpantoUI() to getContent()

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketsList.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketsList.java
@@ -4,14 +4,10 @@ import com.bugTrackerApp.BugTrackerApp.data.entity.Ticket;
 import com.bugTrackerApp.BugTrackerApp.data.service.TicketSystemService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.*;
 
 import javax.annotation.security.RolesAllowed;
-import java.util.List;
-import java.util.UUID;
 
 @PageTitle("Tickets | Bug Tracker")
 @Route(value="tickets")
@@ -25,40 +21,30 @@ public class TicketsList extends VerticalLayout implements HasUrlParameter<Strin
 
     public TicketsList(TicketSystemService TTService) {
         this.TTService = TTService;
-        H1 welcome = new H1("A list of all the tickets");
-       // configureGrid();
-        add(welcome);
     }
 
-
-
-    private void configureGrid() {
-        grid.setSizeFull();
-        grid.setColumns("subject", "dueDate");
-        grid.getColumns().forEach(col -> col.setAutoWidth(true));
-    }
+private void configureGrid() {
+    grid.setSizeFull();
+    grid.setColumns("subject", "dueDate");
+    grid.getColumns().forEach(col -> col.setAutoWidth(true));
+}
 
     @Override
     public void setParameter(BeforeEvent beforeEvent, String name) {
-        // get a list of tickets via repo searching for all tickets by project name
-       // grid.setitems(TTService.findTicketsByProjectName(name))
         this.projectName = name;
-        addSpantoUI();
-//        grid.setItems(TTService.findAllTickets(name));
-        //List<Ticket> tickets =
-       // projectName.setTitle(String.format("You are seeing the %s project", name));
+        getContent();
 
     }
 
-    private void addSpantoUI() {
-        Span displayProjectName = new Span(this.projectName);
+    private void getContent() {
+        H1 welcome = new H1("A list of all the tickets for project: " + this.projectName);
         setSizeFull();
         configureGrid();
         grid.setSizeFull();
-        add(displayProjectName, grid);
+        add(welcome, grid);
         updateGrid();
-
     }
+
 
     private void updateGrid() {
         grid.setItems(TTService.findAllTickets(this.projectName));
