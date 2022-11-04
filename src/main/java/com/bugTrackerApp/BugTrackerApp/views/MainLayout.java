09c6a52bfa88fa547e0cb955ca9c9100825effce
SeemaMistry <seema.m.1996@gmail.com>
2022-11-04 19:47:06 -0400

    chore: clear push and merge tries comments

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
@@ -2,25 +2,6 @@ package com.bugTrackerApp.BugTrackerApp.views;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 
-/*
-* TODO: test out git branches
-*  I am in the master branch
-* I am on the develop branch
-* I am on the feat/TicketTreeGrid branch
-* I am pushing from the feat/TicketTreeGrid branch to the develop branch
-* I  am back to just the feat/TicketTreeGrid branch
-*
-* Pushing changes to develop branch
-* Trying again to push into develop branch
-* Trying attempt 2 to MERGE with develop branch
-*
-* Attempt #3 merge with develop
-*
-* Attempt #4 merge with develop
-*
-* Attempt #5 merger with develop
-*/
-
 public class MainLayout extends AppLayout {
     public MainLayout() {
         createHeader();
