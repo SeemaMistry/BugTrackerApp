2deaf56adff40b2e5f8f8652a7bbcdc306543aa0
SeemaMistry <seema.m.1996@gmail.com>
2022-11-04 10:21:09 -0400

    chore: add to new feat/TicketTreeGrid branch

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.applayout.AppLayout;
 * TODO: test out git branches
 *  I am in the master branch
 * I am on the develop branch
+* I am on the feat/TicketTreeGrid branch
 */
 
 public class MainLayout extends AppLayout {
