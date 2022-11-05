c26751ea881a30bd5252f983e2943f9b3d469268
SeemaMistry <seema.m.1996@gmail.com>
2022-11-04 20:49:14 -0400

    feat: set up basic treegrid plan comments

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketTreeGridView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketTreeGridView.java
@@ -1,4 +1,30 @@
 package com.bugTrackerApp.BugTrackerApp.views.UserViews;
 
-public class TicketTreeGridView {
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import javax.annotation.security.PermitAll;
+
+@Route("Ticket Tree Grid")
+@PermitAll
+public class TicketTreeGridView extends VerticalLayout {
+    // list of tickets
+
+    // services
+    public TicketTreeGridView() {
+        // create new treegrid
+
+        // retrieve tickets from service
+
+        // setItems in the treegrid (tickets, getAssignedEmployees()
+
+        // add HierarchyColumn
+
+        // add remaining columns
+
+        // render treegrid to screen
+    }
+
+    // getAssignedEmployees(Ticket ticket) {}
+
 }
