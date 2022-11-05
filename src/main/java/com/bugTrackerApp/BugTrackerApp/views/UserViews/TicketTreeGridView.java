af07e37b7e986dde643d3c92fdeac477df82e003
SeemaMistry <seema.m.1996@gmail.com>
2022-11-04 20:51:29 -0400

    feat: implement services

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketTreeGridView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketTreeGridView.java
@@ -1,17 +1,26 @@
 package com.bugTrackerApp.BugTrackerApp.views.UserViews;
 
+import com.bugTrackerApp.BugTrackerApp.data.entity.Ticket;
+import com.bugTrackerApp.BugTrackerApp.data.service.TicketSystemService;
+import com.bugTrackerApp.BugTrackerApp.data.service.UserRelationsService;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import javax.annotation.security.PermitAll;
+import java.util.List;
 
 @Route("Ticket Tree Grid")
 @PermitAll
 public class TicketTreeGridView extends VerticalLayout {
     // list of tickets
+    private List<Ticket> tickets;
 
     // services
-    public TicketTreeGridView() {
+    TicketSystemService TSService;
+    UserRelationsService URService;
+    public TicketTreeGridView(UserRelationsService URService, TicketSystemService TSService) {
+        this.TSService = TSService;
+        this.URService = URService;
         // create new treegrid
 
         // retrieve tickets from service
