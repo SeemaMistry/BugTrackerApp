5ecd4928072666ff027a0258a9f362d8d6e9afdc
SeemaMistry <seema.m.1996@gmail.com>
2022-11-26 18:17:00 -0500

    chore: rearrange order of class methods

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketsList.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/TicketsList.java
@@ -4,6 +4,7 @@ import com.bugTrackerApp.BugTrackerApp.data.entity.Ticket;
 import com.bugTrackerApp.BugTrackerApp.data.service.TicketSystemService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.*;
 
@@ -16,25 +17,14 @@ public class TicketsList extends VerticalLayout implements HasUrlParameter<Strin
     TicketSystemService TTService;
     Grid<Ticket> grid = new Grid<>(Ticket.class);
 
+    // search for tickets based on employee ComboBox
+
+
     // store URLParameter name here
     String projectName;
 
     public TicketsList(TicketSystemService TTService) {
         this.TTService = TTService;
-    }
-
-private void configureGrid() {
-    grid.setSizeFull();
-    grid.setColumns("subject", "dueDate");
-    grid.addColumn(e -> e.getTicketReporter().getFullName()).setHeader("Reporter");
-    grid.getColumns().forEach(col -> col.setAutoWidth(true));
-}
-
-    @Override
-    public void setParameter(BeforeEvent beforeEvent, String name) {
-        this.projectName = name.replaceAll("%20", " ");
-        getContent();
-
     }
 
     private void getContent() {
@@ -46,6 +36,19 @@ private void configureGrid() {
         updateGrid();
     }
 
+    private void configureGrid() {
+        grid.setSizeFull();
+        grid.setColumns("subject", "dueDate");
+        grid.addColumn(e -> e.getTicketReporter().getFullName()).setHeader("Reporter");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    @Override
+    public void setParameter(BeforeEvent beforeEvent, String name) {
+        this.projectName = name.replaceAll("%20", " ");
+        getContent();
+
+    }
 
     private void updateGrid() {
         grid.setItems(TTService.findAllTickets(this.projectName));
