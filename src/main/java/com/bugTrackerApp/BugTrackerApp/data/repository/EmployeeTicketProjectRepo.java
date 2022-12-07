ab5a0cf0594228031f614c602f909281aa6f045f
SeemaMistry <seema.m.1996@gmail.com>
2022-12-07 15:01:28 -0500

    chore: comment in all the custom search calls to the EmployeeTicketProject repo

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/EmployeeTicketProjectRepo.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/EmployeeTicketProjectRepo.java
@@ -5,4 +5,11 @@ import com.bugTrackerApp.BugTrackerApp.data.entity.EmployeeTicketProjectKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeTicketProjectRepo extends JpaRepository<EmployeeTicketProject, EmployeeTicketProjectKey> {
+    // find all ETP ROWS by project and employee ids (to get all tickets assigned to employee X)
+
+    // find all ETP ROWS by project and ticket ids (to get employees assigned to ticket Y)
+
+    // find project by ticket and employee ids (find out which project the ticket is from)
+
+    // find projects by employee ids (finds all the projects Employee X is working on tickets for)
 }
