de8f2da58de742f0c77d49f4a60cce77895fe05d
SeemaMistry <seema.m.1996@gmail.com>
2022-12-07 14:00:22 -0500

    feat: implement EmployeeTicketProject entity table with id mappings to composite key

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/EmployeeTicketProject.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/entity/EmployeeTicketProject.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,13 +13,31 @@ import javax.persistence.Entity;
 public class EmployeeTicketProject {
 
     // embedded ETP key
+    @EmbeddedId
+    EmployeeTicketProjectKey employeeTicketProjectKeyId = new EmployeeTicketProjectKey();
 
     // M:1 employee mapping to key
+    @ManyToOne
+    @MapsId("employeeId")
+    @JoinColumn(name = "employeeId")
+    Employee employee;
 
     // M:1 ticket mapping to key
+    @ManyToOne
+    @MapsId("ticketId")
+    @JoinColumn(name = "ticketId")
+    Ticket ticket;
 
     // M:1 project mapping to key
+    @ManyToOne
+    @MapsId("projectId")
+    @JoinColumn(name = "projectId")
+    Project project;
 
     // constructor WITHOUT key
-
+    public EmployeeTicketProject(Employee employee, Ticket ticket, Project project) {
+        this.employee = employee;
+        this.ticket = ticket;
+        this.project = project;
+    }
 }
