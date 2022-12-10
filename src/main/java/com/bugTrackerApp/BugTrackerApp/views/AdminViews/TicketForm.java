c3513695c704538b361243d8a5915b9fc042f7d5
SeemaMistry <seema.m.1996@gmail.com>
2022-12-10 14:22:38 -0500

    chore: comment in ticketForm plan

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/AdminViews/TicketForm.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/AdminViews/TicketForm.java
@@ -4,8 +4,39 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
 
 public class TicketForm extends FormLayout {
+    // Components: TextField, Multiselect ComboBox, ComboBox, Buttons
+
+    // Ticket object
+
+    // bean Binder
+
     public TicketForm() {
+        // bind instance fields
+
+        // configure components
         H1 welcome = new H1("Edit an ticket's info here");
+
+
+        // render to screen
         add(welcome);
     }
+
+    // stream employees selected from multiselect comboBox as a string to TextArea
+
+
+    // button layout and click events
+
+
+    // validate and save ticket
+
+
+    // set a ticket to the form through readBean()
+
+
+    // retrieve selected employees from multiselect comboBox
+
+
+    // EVENT BUS - VAADIN COMPONENT EVENTS
+
+
 }
