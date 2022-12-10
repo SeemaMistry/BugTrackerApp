d6f3e4dd3cb02431df3cf0a9605a897b5c5b44b8
SeemaMistry <seema.m.1996@gmail.com>
2022-12-10 17:51:39 -0500

    feat: initialize constructor and configure all form components

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/AdminViews/TicketForm.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/AdminViews/TicketForm.java
@@ -7,10 +7,13 @@ import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+
+import java.util.List;
 
 public class TicketForm extends FormLayout {
     // Components: TextField, Multiselect ComboBox, ComboBox, Buttons
@@ -21,7 +24,7 @@ public class TicketForm extends FormLayout {
     ComboBox<TicketEstimatedTime> ticketEstimatedTime = new ComboBox<>("Estimated Time");
     ComboBox<TicketType> ticketType = new ComboBox<>("Type");
     ComboBox<Status> ticketStatus = new ComboBox<>("Status");
-    MultiSelectComboBox employeesAssignedToTicket = new MultiSelectComboBox<>("Assigned Employees");
+    MultiSelectComboBox<Employee> employeesAssignedToTicket = new MultiSelectComboBox<>("Assigned Employees");
     TextArea assignedEmployees = new TextArea("Assigned Employees");
     Button save = new Button("Save");
     Button delete = new Button("Delete");
@@ -34,22 +37,54 @@ public class TicketForm extends FormLayout {
     // bean Binder
     Binder<Ticket> binder = new BeanValidationBinder<>(Ticket.class);
 
-    public TicketForm() {
+    public TicketForm(
+            List<Employee> employeeList,
+            List<TicketPriority> ticketPriorityList,
+            List<TicketEstimatedTime> ticketEstimatedTimeList,
+            List<TicketType> ticketTypeList,
+            List<Status> statuses
+    ) {
         // bind instance fields
+        binder.bindInstanceFields(this);
 
         // configure components
         H1 welcome = new H1("Edit an ticket's info here");
+        ticketReporter.setItems(employeeList);
+        ticketReporter.setItemLabelGenerator(Employee::getFullName);
+        ticketPriority.setItems(ticketPriorityList);
+        ticketPriority.setItemLabelGenerator(TicketPriority::getName);
+        ticketEstimatedTime.setItems(ticketEstimatedTimeList);
+        ticketEstimatedTime.setItemLabelGenerator(TicketEstimatedTime::getDescription);
+        ticketType.setItems(ticketTypeList);
+        ticketType.setItemLabelGenerator(TicketType::getName);
+        ticketStatus.setItems(statuses);
+        ticketStatus.setItemLabelGenerator(Status::getName);
 
+        employeesAssignedToTicket.setItems(employeeList);
+        employeesAssignedToTicket.setItemLabelGenerator(Employee::getFullName);
+        assignedEmployees.setReadOnly(true);
+        configureMultiSelectComboBox();
 
         // render to screen
-        add(welcome);
+        add(welcome,
+                subject,
+                dueDate,
+                ticketReporter,
+                ticketPriority,
+                ticketEstimatedTime,
+                ticketType,
+                ticketStatus,
+                employeesAssignedToTicket,
+                assignedEmployees,
+                createButtonLayout()
+        );
     }
 
     // stream employees selected from multiselect comboBox as a string to TextArea
-
+    private void configureMultiSelectComboBox() {}
 
     // button layout and click events
-
+    private HorizontalLayout createButtonLayout() {return new HorizontalLayout();}
 
     // validate and save ticket
 
