65ba8f453d45629821c9c5fb1f7a98909fafc06b
SeemaMistry <seema.m.1996@gmail.com>
2022-12-13 13:47:13 -0500

    feat: implement Query search to search for project by name

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/ProjectRepository.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/ProjectRepository.java
@@ -2,9 +2,16 @@ package com.bugTrackerApp.BugTrackerApp.data.repository;
 
 import com.bugTrackerApp.BugTrackerApp.data.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
     Project findByName(String name);
+
+    @Query("select p from Project p " +
+            "where lower(p.name) like lower(concat('%', :searchTerm, '%'))")
+    List<Project> searchProject(@Param("searchTerm") String searchTerm);
 }
