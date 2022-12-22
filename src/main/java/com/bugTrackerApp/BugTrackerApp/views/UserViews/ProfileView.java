9846c60675c1dcbdb54bf41f1de0c322f5f969d5
SeemaMistry <seema.m.1996@gmail.com>
2022-12-22 18:59:30 -0500

    feat: instansiate ProfileView fields

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/ProfileView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/ProfileView.java
@@ -1,8 +1,14 @@
 package com.bugTrackerApp.BugTrackerApp.views.UserViews;
 
+import com.bugTrackerApp.BugTrackerApp.data.entity.Employee;
+import com.bugTrackerApp.BugTrackerApp.data.service.UserRelationsService;
 import com.bugTrackerApp.BugTrackerApp.views.MainLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -14,9 +20,21 @@ import javax.annotation.security.RolesAllowed;
 @RolesAllowed({"USER", "ADMIN"})
 public class ProfileView extends VerticalLayout {
     // User relation service
+    UserRelationsService URService;
 
     // components
+    TextField firstName = new TextField("First Name");
+    TextField lastName = new TextField("Last Name");
+    EmailField email = new EmailField("Email address");
+    TextField company = new TextField("Company");
+    TextField securityClearance = new TextField("jobTitle");
 
+    // Employee entity
+    Employee employee;
+
+    // binder
+    Binder<Employee> binder = new BeanValidationBinder<>(Employee.class);
+    
     public ProfileView() {
         H1 welcome = new H1("My Profile");
         // hardcode in an employee
