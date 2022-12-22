227f5d4714ebfc12eae3a2f8f2cea299d678a59d
SeemaMistry <seema.m.1996@gmail.com>
2022-12-22 17:24:04 -0500

    feat: implement createHeader() top nav bar

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/MainLayout.java
@@ -1,6 +1,10 @@
 package com.bugTrackerApp.BugTrackerApp.views;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 public class MainLayout extends AppLayout {
     public MainLayout() {
@@ -11,12 +15,22 @@ public class MainLayout extends AppLayout {
 
     private void createHeader() {
         // create Logo (just an H1 tag)
+        H1 logo = new H1("Ticket Tracker");
+        logo.addClassNames("text-l", "m-m");
 
         // create Horizontal layout with a Drawer and logo
+        HorizontalLayout header = new HorizontalLayout(
+                new DrawerToggle(),
+                logo
+        );
 
-        // add stylings
+        // add styling
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.setWidth("100%");
+        header.addClassNames("py-0", "px-m");
 
         // add to nav bar at the top of screen
+        addToNavbar(header);
     }
 
     private void createDrawer() {
