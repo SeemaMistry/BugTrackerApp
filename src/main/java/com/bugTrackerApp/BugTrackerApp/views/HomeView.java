ee95b154ab49c273f92e1e9640be073a6c9edc33
SeemaMistry <seema.m.1996@gmail.com>
2022-12-22 19:50:35 -0500

    fix: remove placeholder code from HomeView

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/HomeView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/HomeView.java
@@ -1,14 +1,7 @@
 package com.bugTrackerApp.BugTrackerApp.views;
 
-import com.bugTrackerApp.BugTrackerApp.data.entity.Company;
-import com.bugTrackerApp.BugTrackerApp.data.service.UserRelationsService;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.value.ValueChangeMode;
+
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -19,35 +12,5 @@ import javax.annotation.security.RolesAllowed;
 @Route(value="", layout = MainLayout.class)
 @PermitAll
 public class HomeView extends VerticalLayout {
-    // TODO: test company search is working
-    // initialize components
-    Grid<Company> grid = new Grid<>(Company.class);
-    TextField filterText = new TextField();
-    UserRelationsService userRelationsService;
 
-    public HomeView(UserRelationsService userRelationsService) {
-        this.userRelationsService = userRelationsService;
-        H1 welcome = new H1("Welcome to Bug Tracker");
-        grid.setColumns("name");
-
-        add(welcome,
-                getToolbar(),
-                grid);
-        updateList();
-    }
-
-    private void updateList() {
-        grid.setItems(userRelationsService.findAllCompanies(filterText.getValue()));
-    }
-
-    private Component getToolbar() {
-        // apply filtertext button behaviours
-        filterText.setPlaceholder("Filter by name...");
-        filterText.setClearButtonVisible(true);
-        filterText.setValueChangeMode(ValueChangeMode.LAZY); // LAZY = will not hit database with every keystroke, wait for a while
-        filterText.addValueChangeListener(e -> updateList());
-        HorizontalLayout toolbar = new HorizontalLayout(filterText);
-        toolbar.addClassName("toolbar");
-        return toolbar;
-    }
 }
