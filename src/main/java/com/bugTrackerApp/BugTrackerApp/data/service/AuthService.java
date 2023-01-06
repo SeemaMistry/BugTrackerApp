9d4f62495ad1f6d6cb6c9df05f791902c12b8a02
SeemaMistry <seema.m.1996@gmail.com>
2023-01-05 23:20:04 -0500

    feat: implement AuthService service layer

---
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.bugTrackerApp.BugTrackerApp.data.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+}
