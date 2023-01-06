1548b8abb89e458ba10ebe78c3d710aa6b57e3f3
SeemaMistry <seema.m.1996@gmail.com>
2023-01-05 23:32:58 -0500

    feat: implement helper methods to set Authorizied Routes based on User Role

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/AuthService.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/AuthService.java
@@ -1,10 +1,20 @@
 package com.bugTrackerApp.BugTrackerApp.data.service;
 
+import com.bugTrackerApp.BugTrackerApp.data.entity.Role;
 import com.bugTrackerApp.BugTrackerApp.data.entity.User;
 import com.bugTrackerApp.BugTrackerApp.data.repository.UserRepository;
+import com.bugTrackerApp.BugTrackerApp.views.AdminViews.EmployeesList;
+import com.bugTrackerApp.BugTrackerApp.views.HomeView;
+import com.bugTrackerApp.BugTrackerApp.views.MainLayout;
+import com.bugTrackerApp.BugTrackerApp.views.UserViews.ProfileView;
+import com.bugTrackerApp.BugTrackerApp.views.UserViews.ProjectsList;
+import com.vaadin.flow.router.RouteConfiguration;
 import com.vaadin.flow.server.VaadinSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -22,6 +32,32 @@ public class AuthService {
             VaadinSession.getCurrent().setAttribute(User.class, user);
             createRoutes(user.getRole());
         }
+    }
+
+    // create Routes based on role
+    private void createRoutes(Role role){
+        // get the role and stream the routes authorized to this role
+        getAuthorizedRoutes(role).stream()
+                .forEach(route -> RouteConfiguration.forSessionScope()
+                        .setRoute(route.path, route.view, MainLayout.class));
+    }
+
+    public List<AuthorizedRoute> getAuthorizedRoutes(Role role) {
+        // create a list of AuthorizedRoutes based on User Role
+        List<AuthorizedRoute> routes = new ArrayList<>();
+
+        if(role.equals(Role.USER)){
+            routes.add(new AuthorizedRoute("", "Home", HomeView.class));
+            routes.add(new AuthorizedRoute("projects", "Projects", EmployeesList.class));
+            routes.add(new AuthorizedRoute("profile", "My Profile", ProfileView.class));
+        } else if(role.equals(Role.ADMIN)) {
+            routes.add(new AuthorizedRoute("", "Home", HomeView.class));
+            routes.add(new AuthorizedRoute("projects", "Projects", ProjectsList.class));
+            routes.add(new AuthorizedRoute("employees", "Employees", EmployeesList.class));
+            routes.add(new AuthorizedRoute("profile", "My Profile", ProfileView.class));
+
+        }
+        return routes;
     }
 
 }
