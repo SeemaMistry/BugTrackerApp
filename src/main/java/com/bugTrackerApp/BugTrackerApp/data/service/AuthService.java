8e6d6d3a419496ee7c4da338326b7adb9e01a537
SeemaMistry <seema.m.1996@gmail.com>
2023-01-05 23:26:34 -0500

    feat: implement authenticate() method to retrieve User from the database if they exist and set Session and Routes based on roles

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/AuthService.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/service/AuthService.java
@@ -1,5 +1,8 @@
 package com.bugTrackerApp.BugTrackerApp.data.service;
 
+import com.bugTrackerApp.BugTrackerApp.data.entity.User;
+import com.bugTrackerApp.BugTrackerApp.data.repository.UserRepository;
+import com.vaadin.flow.server.VaadinSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class AuthService {
+    UserRepository userRepository;
+
+
+    // retrieve User from the database if they exist and set Session and Routes
+    public void authenticate(String username, String password){
+        // retrieve user from database
+        User user = userRepository.getByUsername(username);
+        // if user exists, set session and create routes
+        if (user != null && user.checkPassword(password)){
+            VaadinSession.getCurrent().setAttribute(User.class, user);
+            createRoutes(user.getRole());
+        }
+    }
 
 }
