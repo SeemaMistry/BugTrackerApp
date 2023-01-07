3a346802bc0564df67977bf9c14fa83ddad5cd1e
SeemaMistry <seema.m.1996@gmail.com>
2023-01-06 20:17:37 -0500

    feat: invalidate and close current session and redirecct to the login page

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/LogoutViews/LogOut.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/LogoutViews/LogOut.java
@@ -1,9 +1,11 @@
 package com.bugTrackerApp.BugTrackerApp.views.LogoutViews;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 
 import javax.annotation.security.PermitAll;
 
@@ -13,6 +15,14 @@ import javax.annotation.security.PermitAll;
 public class LogOut extends VerticalLayout {
 
     public LogOut() {
+        // return page to login page
+        UI.getCurrent().getPage().setLocation("login");
+
+        // invalidate and close current session
+        VaadinSession.getCurrent().getSession().invalidate();
+        VaadinSession.getCurrent().close();
+
+
         H1 welcome = new H1("You have logout!");
         add(welcome);
     }
