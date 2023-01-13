9ef6dd96d7dab440a2b3a7252f5486c8a83ee87b
SeemaMistry <seema.m.1996@gmail.com>
2023-01-12 20:50:34 -0500

    refactor: update view of my profile page into an Accodion for better usability and organization

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/ProfileView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/UserViews/ProfileView.java
@@ -4,9 +4,12 @@ import com.bugTrackerApp.BugTrackerApp.data.entity.Employee;
 import com.bugTrackerApp.BugTrackerApp.data.entity.User;
 import com.bugTrackerApp.BugTrackerApp.data.service.UserRelationsService;
 import com.bugTrackerApp.BugTrackerApp.views.MainLayout;
+import com.vaadin.flow.component.accordion.Accordion;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
@@ -41,6 +44,11 @@ public class ProfileView extends VerticalLayout {
     Button save = new Button("Save");
     Button cancel  = new Button("Cancel");
 
+    Accordion accordion = new Accordion();
+    PasswordField currentPassword2 = new PasswordField("Current Password");
+    PasswordField newPassword2 = new PasswordField("New Password");
+    PasswordField confirmNewPassword2 = new PasswordField("Confirm New Password");
+
     // Employee entity
     Employee employee;
 
@@ -52,11 +60,12 @@ public class ProfileView extends VerticalLayout {
         User user = VaadinSession.getCurrent().getAttribute(User.class);
         this.employee = user.getEmployee();
 
-        // configure my profile components
-        configureComponents();
-        configureChangePasswordBtns();
+        // configure accordion
+        createAccordion();
+        // configure change password save and cancel events
+        changeBtnSaveAndCancelEvents();
 
-        add(welcome, getContent());
+        add(welcome, this.accordion);
     }
 
     // configure component values and width to 300px
@@ -88,8 +97,8 @@ public class ProfileView extends VerticalLayout {
         this.currentPassword.setVisible(false);
         this.newPassword.setVisible(false);
         this.confirmNewPassword.setVisible(false);
-        this.save.setVisible(false);
-        this.cancel.setVisible(false);
+        this.save.setVisible(true);
+        this.cancel.setVisible(true);
     }
 
     // set components in a vertical layout
@@ -117,9 +126,13 @@ public class ProfileView extends VerticalLayout {
             this.newPassword.setVisible(true);
             this.confirmNewPassword.setVisible(true);
             this.save.setVisible(true);
-            this.cancel.setVisible(false);
+            this.cancel.setVisible(true);
         });
 
+        changeBtnSaveAndCancelEvents();
+    }
+
+    private void changeBtnSaveAndCancelEvents(){
         this.save.addClickListener(e -> {
             // validate and save the password
             boolean passwordChangeSuccess = validateAndSave();
@@ -129,25 +142,62 @@ public class ProfileView extends VerticalLayout {
 
         this.cancel.addClickListener(e->{
             // close the password fields
-            this.currentPassword.setVisible(false);
-            this.newPassword.setVisible(false);
-            this.confirmNewPassword.setVisible(false);
+            this.currentPassword2.setValue("");
+            this.newPassword2.setValue("");
+            this.confirmNewPassword2.setValue("");
         });
     }
 
     private boolean validateAndSave(){
         // check if currentPassword == current password
-        if (this.employee.getUserAccountDetail().checkPassword(this.currentPassword.getValue())){
+        if (this.employee.getUserAccountDetail().checkPassword(this.currentPassword2.getValue())){
             // check if newPassword == confirmNewPassword
-            if (this.newPassword.getValue().equals(this.confirmNewPassword.getValue())){
+            if (this.newPassword2.getValue().equals(this.confirmNewPassword2.getValue())){
                 // change and save new password
                 User user = this.employee.getUserAccountDetail();
-                user.changePassword(confirmNewPassword.getValue());
+                user.changePassword(confirmNewPassword2.getValue());
                 URService.saveUser(user);
                 return true;
             }
         }
         // else return false
         return false;
+    }
+
+    public void createAccordion(){
+        // set personal info
+        Span fullName = new Span(this.employee.getFullName());
+        Span email = new Span(this.employee.getEmail());
+        VerticalLayout personalInfo = new VerticalLayout(fullName, email);
+
+        // set account info
+        Span username = new Span("Username: " + this.employee.getUserAccountDetail().getUsername());
+        Span password = new Span("Password: " + "********");
+        VerticalLayout accountInfo = new VerticalLayout(username, password);
+
+        // set change password info: password fields to change
+        Span description = new Span("Change your password below:");
+        VerticalLayout changePasswordInfo = new VerticalLayout(
+                description,
+                this.currentPassword2,
+                this.newPassword2,
+                this.confirmNewPassword2,
+                new HorizontalLayout(this.save, this.cancel)
+        );
+
+        // set company info
+        Span companyName = new Span(this.employee.getCompany().getName());
+        Span jobTitle = new Span(this.employee.getSecurityClearance().getSecurityTitle());
+        VerticalLayout companyInfo = new VerticalLayout(companyName, jobTitle);
+
+        // set accordion panels
+        // Personal info: name, email
+        this.accordion.add("Personal information", personalInfo);
+        // Account info: username, password .. change password?
+        this.accordion.add("Account information", accountInfo);
+        // Account info: username, password .. change password?
+        this.accordion.add("Change password", changePasswordInfo);
+        // Company info:  company, job title
+        this.accordion.add("Company information", companyInfo);
     }
 }
