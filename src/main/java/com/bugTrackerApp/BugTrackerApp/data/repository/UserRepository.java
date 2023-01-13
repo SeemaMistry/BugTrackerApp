db6fd391a951d3f8fcce68c3e5f5eddc0f4292e0
SeemaMistry <seema.m.1996@gmail.com>
2023-01-12 20:07:56 -0500

    chore: clean up code

--- src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/UserRepository.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/data/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.bugTrackerApp.BugTrackerApp.data.repository;
 
 import com.bugTrackerApp.BugTrackerApp.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
     User getByUsername(String username);
 }
