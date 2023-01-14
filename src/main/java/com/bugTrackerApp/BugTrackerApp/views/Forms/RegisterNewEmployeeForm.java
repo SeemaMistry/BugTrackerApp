2e56d914721b15d7b3c1a96ffc45b726b7b41dc4
SeemaMistry <seema.m.1996@gmail.com>
2023-01-13 19:30:21 -0500

    fix: addValueChangeListener to firstName to autopopulate username and password TextFields

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/Forms/RegisterNewEmployeeForm.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/Forms/RegisterNewEmployeeForm.java
@@ -2,6 +2,7 @@ package com.bugTrackerApp.BugTrackerApp.views.Forms;
 
 import com.bugTrackerApp.BugTrackerApp.data.entity.Company;
 import com.bugTrackerApp.BugTrackerApp.data.entity.SecurityClearance;
+import com.bugTrackerApp.BugTrackerApp.data.service.UserRelationsService;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 
@@ -11,19 +12,21 @@ public class RegisterNewEmployeeForm extends EmployeeForm{
     TextField username = new TextField("Username");
     TextField password = new TextField("Password");
 
-    public RegisterNewEmployeeForm(List<Company> companies, List<SecurityClearance> securityClearances) {
-        super(companies, securityClearances);
+    public RegisterNewEmployeeForm(List<Company> companies, List<SecurityClearance> securityClearances, UserRelationsService URService) {
+        super(companies, securityClearances, URService);
         updateUsernameAndPassword();
         add(username, password);
+        updateUsernameAndPassword();
 
     }
 
     private void updateUsernameAndPassword(){
-
-        username.setValueChangeMode(ValueChangeMode.EAGER);
-
-        password.setValueChangeMode(ValueChangeMode.EAGER);
-        username.setValue(super.firstName.getValue());
-        password.setValue(super.firstName.getValue());
+//        super.firstName.setValueChangeMode(ValueChangeMode.ON_CHANGE);
+        super.firstName.addValueChangeListener(e -> {
+            // when value changes, set it as username and password
+            username.setValue(e.getValue());
+            password.setValue(e.getValue());
+        });
     }
+
 }
