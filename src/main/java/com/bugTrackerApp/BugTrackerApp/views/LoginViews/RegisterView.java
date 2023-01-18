389433c7ec9ab1371e8d16770c9bcbc4ce2d0d0c
SeemaMistry <seema.m.1996@gmail.com>
2023-01-17 20:31:15 -0500

    feat: create plan for registerView.class methods

--- src/main/java/com/bugTrackerApp/BugTrackerApp/views/LoginViews/RegisterView.java
+++ src/main/java/com/bugTrackerApp/BugTrackerApp/views/LoginViews/RegisterView.java
@@ -1,6 +1,10 @@
 package com.bugTrackerApp.BugTrackerApp.views.LoginViews;
 
+import com.bugTrackerApp.BugTrackerApp.data.repository.UserRepository;
+import com.bugTrackerApp.BugTrackerApp.data.service.UserRelationsService;
+import com.bugTrackerApp.BugTrackerApp.views.Forms.RegisterForm;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -11,19 +15,49 @@ import javax.annotation.security.PermitAll;
 @Route(value="register")
 @PermitAll
 public class RegisterView extends VerticalLayout {
+    // services
+    UserRelationsService URService;
+
+    // components
+    RegisterForm registerForm;
+
     public RegisterView() {
-        H1 welcome = new H1("Register a new account");
-        add(welcome);
+        H1 welcome = new H1("Register a New Account");
+        Span instructions = new Span("Create a new ADMIN account for a new company below.\n" +
+                "If you are trying to create an account under an EXISTING company, please contact your company " +
+                "administers to create register your new user account");
+
+        // configure form
+        configureForm();
+        add(welcome, instructions);
     }
 
     /* ------------------- CONFIGURATIONS -------------------
      * */
 
+    // configure registerForm
+    private void configureForm(){
+        registerForm = new RegisterForm();
+    }
 
     /* ------------------- GET COMPONENTS -------------------
      * */
 
+    // return form
+    private void getContent(){
+    }
+
     /* ------------------- FORM MANIPULATIONS -------------------
-     * add new account, save, delete, clear, open and close
+     * add new account, save, cancel, clear
      * */
+
+    // save new company and employee
+    private void saveAccount(){}
+
+    // cancel redirects to welcome page
+    private void cancel(){}
+
+    // clear values in form
+    private void clearForm(){}
+
 }
